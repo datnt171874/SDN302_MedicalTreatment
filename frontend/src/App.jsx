5f09b78ddfea64db382dc7825292f33b96420187
LAPTOP-PC94J89O\ASUS <tudvtse183314@fpt.edu.vn>
2025-06-12 09:40:27 +0700

    add menu for doctor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,20 @@ import Header from "./components/Header";
 import TreatmentPlanUser from "./pages/TreatmentPlanUser";
 import TreatmentPlanDoctor from "./pages/TreatmentPlanDoctor";
 
+// Doctor Pages
+import DoctorDashboard from "./pages/doctor/DoctorDashboard";
+import DoctorPatients from "./pages/doctor/DoctorPatients";
+import DoctorPatientDetail from "./pages/doctor/DoctorPatientDetail";
+import DoctorAppointments from "./pages/doctor/DoctorAppointments";
+import DoctorReminders from "./pages/doctor/DoctorReminders";
+import DoctorProfile from "./pages/doctor/DoctorProfile";
+
 function App() {
-  const location = useLocation(); // Get current location
+  const location = useLocation();
 
   return (
     <>
-      {location.pathname !== '/user' && <Header />} {/* Conditionally render Header */}
+      {location.pathname !== '/user' && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/checkin" element={<CheckinPage />} />
@@ -25,9 +33,17 @@ function App() {
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/doctor" element={<DoctorPage />} />
         <Route path="/user" element={<User />} />
-        <Route path="/treatmentUser" element={<TreatmentPlanUser/>} />
-        <Route path="/treatmentDoctor" element={<TreatmentPlanDoctor/>} />
+        <Route path="/treatmentUser" element={<TreatmentPlanUser />} />
+        <Route path="/treatmentDoctor" element={<TreatmentPlanDoctor />} />
 
+        {/* Doctor routes */}
+        <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+        <Route path="/doctor/patients" element={<DoctorPatients />} />
+        <Route path="/doctor/patient/:id" element={<DoctorPatientDetail />} />
+        <Route path="/doctor/appointments" element={<DoctorAppointments />} />
+        <Route path="/doctor/treatments" element={<TreatmentPlanDoctor />} />
+        <Route path="/doctor/reminders" element={<DoctorReminders />} />
+        <Route path="/doctor/profile" element={<DoctorProfile />} />
       </Routes>
     </>
   );
